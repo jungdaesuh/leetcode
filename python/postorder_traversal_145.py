91321ee8e1dee930ddcc3e97d5e29b097b2e6d79
JungDaeSuh <78460559+JungDaeSuh@users.noreply.github.com>
2022-03-14 00:02:07 +0900

    iterative solution + recursive solution

---
+++ python/postorder_traversal_145.py
@@ -0,0 +1,17 @@
+def iterative_postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+
+
+    res, stack = [], [root]
+    while stack:
+        node = stack.pop()
+        if node:
+            res.append(node.val)
+            stack.append(node.left)
+            stack.append(node.right)
+    return res[::-1]
+
+def recursive_postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+
+        return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
